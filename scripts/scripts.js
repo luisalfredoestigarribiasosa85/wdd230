187d4dac4f3e752168473ba071da9362770180e9
luisalfredoestigarribiasosa85 <luisalfredoestigarribiasosa85@gmail.com>
2024-04-01 20:43:54 -0400

    Home page enhancement

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,6 +2,7 @@ const hamburgerElement = document.querySelector('#myButton');
 const navElement = document.querySelector('.menuLinks');
 const darkMode = document.querySelector('#darkMode');
 const mainDark = document.querySelector('main');
+const visits = document.querySelector('#visits');
 
 hamburgerElement.addEventListener('click', () => {
     // toggle class name on click of the button
@@ -12,3 +13,12 @@ hamburgerElement.addEventListener('click', () => {
 darkMode.addEventListener('click', () => {
     mainDark.classList.toggle('dark');
 });
+
+let numVisits = Number(window.localStorage.getItem('numVisits-ls')) || 0;
+if (numVisits !== 0) {
+    visits.textContent = `Visits: ${numVisits}`;
+} else {
+    visits.textContent = 'Welcome. This is your first visit'
+}
+numVisits++;
+window.localStorage.setItem('numVisits-ls', numVisits);
